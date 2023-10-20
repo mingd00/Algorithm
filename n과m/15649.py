21b3ede03b829ad3d4b958a104517a205acacbed
mingd00 <rkdalswl403@naver.com>
2023-10-20 11:28:05 +0900

    [fix] 함수 이름 변경

--- n과m/15649.py
+++ n과m/15649.py
@@ -1,7 +1,7 @@
 import sys 
 input = sys.stdin.readline
 
-def nm1(vlist) :
+def permutation(vlist) :
     if len(vlist) == m:
         print(*vlist)
         return
@@ -9,14 +9,14 @@ def nm1(vlist) :
     for i in range(n):
         if visited[i] == 0:
             visited[i] = 1
-            nm1(vlist+[i+1])
+            permutation(vlist+[i+1])
             visited[i] = 0   
             
 if __name__ == "__main__" :
     vlist = []
     n, m = map(int, input().split())
     visited = [0 for _ in range(n)]
-    nm1(vlist)
+    permutation(vlist)
     
     
     
--- n과m/15651.py
+++ n과m/15651.py
@@ -1,15 +1,15 @@
 import sys 
 input = sys.stdin.readline
 
-def nm3(vlist) :
+def permutation_same_number_possible(vlist) :
     if len(vlist) == m:
         print(*vlist)
         return
     
     for i in range(n):
-        nm3(vlist + [i+1])
+        permutation_same_number_possible(vlist + [i+1])
 
 if __name__ == "__main__" :
     vlist = []
     n, m = map(int, input().split())
-    nm3(vlist)
+    permutation_same_number_possible(vlist)

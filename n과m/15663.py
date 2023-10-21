ac27f56facdd1ea9d3f9084083bd61716e07a9fd
mingd00 <rkdalswl403@naver.com>
2023-10-21 23:56:11 +0900

    [feat] 조건에 맞는 수열 출력
    - 중복으로 숫자 입력(n개의 숫자 m자릿수)
    - 수열을 조건을 맞춰 나열할 때 중복되면 안됨

---
+++ n과m/15663.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+def permutation_input_same_number(vlist, visited):
+    if len(vlist) == m:
+        print(*vlist)
+        return
+    
+    for i in range(n):
+        if visited[i] == 0:
+            visited[i] = 1
+            permutation_input_same_number(vlist + [sorted_list[i]], visited)
+            visited[i] = 0
+
+def sort_list(input_list):
+    sorted_list = sorted(input_list)
+    return sorted_list
+
+if __name__ == "__main__":
+    vlist = []
+    n, m = map(int, input().split())
+    input_list = list(map(int,input().split()))
+    sorted_list = sort_list(input_list)
+    visited = [ 0 for _ in range(n) ]
+    permutation_input_same_number(vlist, visited)

5ed6a4c1d7681fe1aa54e63f0a409611ef57263d
mingd00 <rkdalswl403@naver.com>
2023-10-22 20:43:31 +0900

    [feat] 조합 입력 중복
    - n까지의 수로 m자릿수 조합하기
    - 중복X

---
+++ n과m/15664.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+def combination_input_same_number(vlist, index):
+    if len(vlist) == m:
+        answer.append(vlist)
+        return
+    
+    for i in range(index, n):
+        combination_input_same_number(vlist + [input_list[i]], i + 1)
+
+if __name__ == "__main__":
+    index = 0
+    vlist = []
+    answer = []
+    n, m = map(int, input().split())
+    input_list = list(map(int, input().split()))
+    input_list.sort()
+    combination_input_same_number(vlist, index)
+    set_of_tuple = sorted(list(set(map(tuple, answer))))
+    for i in set_of_tuple:
+        print(*i)
+    
+    
+    

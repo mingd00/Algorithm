d529e54ab0401841a2fc3f98a42a4f2be6f9f063
mingd00 <rkdalswl403@naver.com>
2023-10-23 15:50:45 +0900

    [fix] 조합 및 변수 수정

--- n과m/15650.py
+++ n과m/15650.py
@@ -13,8 +13,20 @@ def permutation_asc(vlist) :
             permutation_asc(vlist + [i+1])
             visited[i] = 0
             
+def combination(vlist, index) :
+    if len(vlist) == m:
+        print(*vlist)
+        return
+    
+    for i in range(index, n):
+        if visited[i] == 0:
+            visited[i] = 1
+            combination(vlist + [i+1], i+1)
+            visited[i] = 0
+            
 if __name__ == "__main__" :
+    index = 0
     vlist = []
     n, m = map(int, input().split())
     visited = [0 for i in range(n)]
-    permutation_asc(vlist)
+    combination(vlist, index)
--- n과m/15652.py
+++ n과m/15652.py
@@ -2,16 +2,16 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def combination(vlist, index) :
+def combination_same_number_possible(vlist, index) :
     if len(vlist) == m: 
         print(*vlist)
         return
     
     for i in range(index, n):
-        combination(vlist + [i+1], i)
+        combination_same_number_possible(vlist + [i+1], i)
 
 if __name__ == "__main__" :
     vlist = []
     index = 0
     n, m = map(int, input().split())
-    combination(vlist, index)
+    combination_same_number_possible(vlist, index)

0c4cdaa205a3ef7082de2c106e72e0fabaac0467
mingd00 <rkdalswl403@naver.com>
2024-01-10 15:48:34 +0900

    [feat] 암호 만들기
    - 백트래킹
    - dfs

---
+++ 백트래킹/암호만들기_1759.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(start, answer, L, C, words, vowel):
+    # 모음
+    vowel_count = 0
+    # 자음
+    consonant_count = 0
+    
+    if len(answer) == L:
+        for i in answer:
+            if i in vowel:
+                vowel_count += 1
+            else:
+                consonant_count += 1
+                
+        if vowel_count >= 1 and consonant_count >= 2:
+            print(''.join(answer))
+            
+        return
+    
+    for i in range(start, C):
+        answer.append(words[i])
+        dfs(i+1, answer, L, C, words, vowel)
+        answer.pop()
+         
+def main():
+    # 모음
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    # 정답
+    answer = []
+    # 입력
+    L, C = map(int, input().split())
+    words = sorted(list(input().rstrip().split()))
+    
+    dfs(0, answer, L, C, words, vowel)
+ 
+if __name__ == "__main__":
+    main()

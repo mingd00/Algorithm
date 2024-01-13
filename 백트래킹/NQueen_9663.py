217013126a0a467b0ba108411840e37df1094f93
mingd00 <rkdalswl403@naver.com>
2024-01-13 22:52:51 +0900

    [feat] N-Queen
    - 백트래킹
    - pypy

--- 백트래킹/NQueen_9663.py
+++ 백트래킹/NQueen_9663.py
@@ -1,9 +1,33 @@
 import sys 
 input = sys.stdin.readline
 
-def main() :
-    n = int(input())
-    pass
+# 행 단위로 탐색
+def dfs(row, N, v1, v2, v3):
+    global answer
+    
+    if row == N:
+        answer += 1
+        return
+    
+    for col in range(N):
+        if v1[col] == v2[row - col] == v3[row + col] == 0:
+            v1[col] = v2[row - col] = v3[row + col] = 1
+            dfs(row+1, N, v1, v2, v3)
+            v1[col] = v2[row - col] = v3[row + col] = 0     
 
-if __name__ == "__main__" : 
+def main() :
+    N = int(input())
+    
+    # 열 방문 체크
+    v1 = [0] * N
+    # 왼쪽 위 대각선 방문 체크
+    v2 = [0] * (2*N)
+    # 오른쪽 위 대각선 방문 체크
+    v3 = [0] * (2*N)
+    
+    dfs(0, N, v1, v2, v3)
+
+if __name__ == "__main__" :
+    answer = 0
     main()
+    print(answer)

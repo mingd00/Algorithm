f2cb95c2464f74340330f7546784f23c4196d268
mingd00 <rkdalswl403@naver.com>
2025-04-08 23:38:54 +0900

    25/04/08

--- 구현/괄호의값_2504.py
+++ 구현/괄호의값_2504.py
@@ -1,9 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-def cal(n):
-    pass
+def cal(s):
+    answer = 0
+    tmp = 1
+    stack = []
+    
+    for i in range(len(s)):
+        if string[i] == '(':
+            stack.append('(')
+            tmp *= 2
+        elif string[i] == '[':
+            stack.append('[')
+            tmp *= 3
+        elif string[i] == ')':
+            if not stack or stack[-1] == '[':
+                answer = 0
+                break
+            if string[i-1] == '(':
+                answer += tmp
+            stack.pop()
+            tmp //= 2
+        else:
+            if not stack or stack[-1] == '(':
+                answer = 0
+                break
+            if string[i-1] == '[':
+                answer += tmp
+            stack.pop()
+            tmp //= 3
+    
+    if stack:
+        print(0)
+    else:
+        print(answer)
         
 if __name__ == "__main__":
-    string = input().rstrip()
-    print(string)
+    string = list(input().rstrip())
+    cal(string)
